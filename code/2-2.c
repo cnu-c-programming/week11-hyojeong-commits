7498c59aa60776588814dd58349662b69765e7b4
hyojeong-commits <kweunsungo@gmail.com>
2026-05-19 14:46:03 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 enum Color {RED, GREEEN, BLUE};
+
 enum Status { OK=0, ERR = -1, BUSY =1};
 
 int main(void) {
@@ -8,9 +9,9 @@ int main(void) {
   printf("GREEN = %d\n", GREEN);
   printf("BLUE  = %d\n", BLUE);
 
-  printf("OK   = %d\n", OK);
-  printf("ERR  = %d\n", ERR);
-  printf("BUSY = %d\n", BUSY);
+  printf("OK    = %d\n", OK);
+  printf("ERR   = %d\n", ERR);
+  printf("BUSY  = %d\n", BUSY);
 
     return 0;
 }
