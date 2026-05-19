70a27be334391ef7838af14b33cb41d3f2671d5d
hyojeong-commits <kweunsungo@gmail.com>
2026-05-19 13:35:47 +0900

    Update 3-2.c

---
+++ code/3-2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void print_arr(int *arr, int size) {
+    for (int i=0; i<size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main(void) {
+    int arr[]={5,2,8,1,9,3};
+    int size=6;
+
+    qsort;
+    print_arr(arr, size);
+
+    qsort);
+    print_arr(arr, size);
+
+    return 0;
+}
