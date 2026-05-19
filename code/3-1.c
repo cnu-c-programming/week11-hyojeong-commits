52737fbbbec5a3b572d3f86bd8a7254746a77007
hyojeong-commits <kweunsungo@gmail.com>
2026-05-19 14:39:02 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -2,13 +2,19 @@
 
 typedef int (*BinOp)(int, int);
 
-int add(int a, int b) { return a + b; }
-int sub(int a, int b) { return a - b; }
-int mul(int a, int b) { return a * b; }
+int add(int a, int b){return a+b;}
+int sub(int a, int b){return a-b;}
+int mul(int a, int b){return a*b;}
 
-int compute
-int main(void) {
-    int a = 10, b = 5;
+int compute(int a,int b,BinOp op){
+    if(op==NULL) {
+        printf("Null pointer\n");
+        return -1;
+    }
+    return op(a, b);
+}
+int main(void){
+    int a=10, b=5;
 
     printf("%d\n", compute(a, b, add));
     printf("%d\n", compute(a, b, sub));
